5b331df729460c8c4213aa6eec65825e03a7538a
BerNardEr <gorgilike@mail.ru>
2017-10-21 17:29:45 +0300

    rev-3
    Command arguments;
    Inline interpreter commands usage with "\command argument\";
    Interpreter reset;

--- APP.cpp
+++ APP.cpp
@@ -6,9 +6,10 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#define BUFCAP 4096
 using namespace std;
-double c[256];
-string sc[256];
+double c[BUFCAP];
+string sc[BUFCAP];
 int ide = 0;
 int u = 0;
 int ech = 0;
@@ -17,6 +18,18 @@ string cbuf;
 
 void op (string arg);
 void pxtc(string arg);
+void reset(){
+	mode = 0;
+	ech=0;
+	memset(c, 0, sizeof(c));
+	u = 0;
+	int i = 0;
+	while (i<BUFCAP){
+		sc[i]="";
+		i++;
+	}
+	return;
+}
 string gt(){
 	string arg;
 	getline(std::cin,arg);
@@ -73,7 +86,35 @@ string readFile(string path){
     return result;
 }
 
+string split(string s, string delimiter, int numr){
+s+=delimiter;
+string trues = s;
+size_t pos = 0;
+int i =0;
+string token;
+while ((pos = s.find(delimiter)) != std::string::npos) {
+    i++;
+    s.erase(0, pos + delimiter.length());
+}
+s = trues;
+string ret[i];
+i = 0;
+while ((pos = s.find(delimiter)) != std::string::npos) {
+    token = s.substr(0, pos);
+    ret[i] = token;
+    i++;
+    s.erase(0, pos + delimiter.length());
+}
+return ret[numr];
+}
+
 bool cmds(string arg){
+	string bufa = arg;
+	arg = split(arg," ",0);
+	if (arg == "reset"){
+		reset();
+		return true;
+	}
 	if (arg == "rb"){
 		op(cbuf);
 		return true;
@@ -90,28 +131,24 @@ bool cmds(string arg){
 		return true;
 	}
 	if (arg=="save"){
-		string fn;
-		cout<<"ENTER FILENAME: ";
-		getline(std::cin, fn);
+		string fn = split(bufa," ",1);
 		fn+=".appl";
 		writeFile(fn,cbuf);
-		cout<<"Saved your program (only last command buffer)";
+		cout<<"Saved your program (only last command buffer) to "+fn;
 		return true;
 	}
 	if (arg=="load"){
-		cout<<"ENTER FILENAME TO LOAD:";
-		cbuf = readFile(gt()+".appl");
+		cbuf = readFile(split(bufa," ",1)+".appl");
 		cout<<"\nLoaded code to your command buffer!";
 		return true;
 	}
 	if (arg=="run"){
-		cout<<"ENTER FILENAME TO RUN:";
-		op(readFile(gt()+".appl"));
+		op(readFile(split(bufa," ",1)+".appl"));
 		cout<<"\nFinished run from file!";
 		return true;
 	}
 	if (arg=="help"){
-		cout<<"INTERPRETER COMMANDS:\n**********\nech - debug mode on/of\nrb - run commands from command buffer (last used)\nsave - save command buffer to file\nload - load file to command buffer\nrun - run file\ncls - clear screen\n\nDou you want to see operators list? (y/n): ";	
+		cout<<"INTERPRETER COMMANDS:\n**********\nech - debug mode on/of\nrb - run commands from command buffer (last used)\nsave <filename> - save command buffer to file\nload <filename> - load file to command buffer\nrun <filename> - run file\ncls - clear screen\n\nDou you want to see operators list? (y/n): ";	
 		if (gt()=="y"){
 		cout<<"\n**********\nOPERATORS:\n**********\n";
 		cout<<"a\nSets current int cell to 0 if MODE is 0 and if MODE is 1, clears current string cell\n\np\nIncrements current cell\n\nm\nDecrements current cell\n\nw\nWrites current int cell value if current mode is 0 and writes current string cell value if mode is 1\n\n_\nEchoes end of line\n\n>\nNext cell\n\n<\nPrevious cell\n\n.\nPuts a symbol with code from current int cell to current string cell\n\nv\nAdds 5 to current cell\n\nx\nAdds 10 to current cell\n\ni\nIf MODE is 0, gets int from keyboard to int cell, if MODE is 1, gets string from keyboard.\n\n+\nSets value of current cell to sum of two previous cells (cell[current] = cell[current-2] + cell[current-1])\n\n-\nSets value of current cell to cell[current-2] - cell[current-1]\n\n?\nIf cell[current-2] == cell[current-1], sets current cell to 1, otherwise, to 0.\n\n g\nIf cell[current-2] > cell[current-1], sets current cell to 1, otherwise, to 0.\n\n s\nIf cell[current-2] < cell[current-1], sets current cell to 1, otherwise, to 0.\n\n r\nSets current cell value to random int in range min = cell[current-2] & max = cell[current-1]\n\n{\nRepeats 1 operator after it cell[current-1] times\n\n!\nExecutes next operator if cell[current-1] == 1\n\nc\nPrints current cell number\n\nS\nSwitches MODE between 0 (int) and 1 (string)\n\n/\nMakes cell[current] = cell[current-2] / cell[current-1]\n\nR\nMakes cell[current] = cell[current-2] / cell[current-1]\n";
@@ -122,19 +159,29 @@ bool cmds(string arg){
 void op(string arg)
 {
 	int echof = 0;
-if(ide==0){
 	int i = 0;
 	int nn = 0;
-	
 	if (cmds(arg))
 	return;
 	nn = arg.size();
 	string op;
+	string scmd = "";
 	cbuf = arg;
-	while (i < nn)
-	{
+	bool iscmd = false;
+	try{
+	while (i < nn){
 		op = arg.substr(i, 1);
-		
+		if (op == "\\"){
+			if (iscmd) iscmd = false;
+			else iscmd = true;
+		}
+		if (!iscmd && scmd!=""){
+			cmds(scmd);
+			scmd = "";
+		}
+		if (iscmd && op!="\\"){
+			scmd+=op;
+		}else{
 		if (op == "\"")
 			if (echof == 1)
 			echof = 0;
@@ -288,12 +335,15 @@ if(ide==0){
 				cout << endl << "UNKNOWN OP: '" + op + "'" << endl;
 		}
 	}
+	}
 		i++;
 	}
-	}
-	else{
-	cout <<"OP: "<<spl(arg,0);
-	}
+}
+catch (std::exception &e){
+	cout<<"AN ERROR HAS OCCURED\nRESET...";
+	reset();
+	return;
+}
 }
 
 void pxtc(string arg)
@@ -304,7 +354,7 @@ void pxtc(string arg)
 main()
 {
 	string in;
-	echo("APP CONSOLE INTERPRETER \nType help to see the list of avalaible commands");
+	echo("APP CONSOLE INTERPRETER \nType 'help' to see the list of avalaible commands");
 	while (true)
 	{
 		cout << endl << ">> ";
